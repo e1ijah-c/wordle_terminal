fa97a12d75d359fed34112e827e185ae32b5d407
Elijah Chia <elcxd99@gmail.com>
2023-10-06 10:48:32 +0800

    updated stuff

--- WordlePy.py
+++ WordlePy.py
@@ -1,13 +1,35 @@
+import random
+from random import randint
+import colorama
+from colorama import Fore, Style
+
+
+wordbank = []
+wordle_char = []
+guess_char = []
+num_terms = ["FIRST", "SECOND", "THIRD", "FOURTH", "FIFTH", "LAST"]
+
+guess = ''
+turns = 6
+n = 0
+
+
+with open("Wordle_Dictionary.txt") as file:
+    words = file.readlines()
+
+    for word in words:
+        wordbank.append(word.strip('\n'))
+
 while True:
 
     try:
 
-        print("Welcome to Wordle but in python.")
+        print(Fore.RED + "Welcome to Wordle but in python.")
         print(" ")
         print("If you would like to know the rules, input '1'.")
         print("If you would like to play, input '2'.")
         print(" ")
-        user_input = int(input("Enter your selected option:"))
+        user_input = int(input("Enter your selected option: "))
 
         if user_input == 1:
             print(".")
@@ -15,11 +37,11 @@ while True:
             print(".")
             print(".")
             print(".")
-            print("You have to guess the Wordle in six goes or less.")
+            print("You have to guess the Wordle in 6 goes or less.")
             print("Every word you enter must be in the word list.")
-            print("A correct letter turns green.")
-            print("A correct letter in the wrong place turns yellow.")                
-            print("An incorrect letter turns gray.")
+            print("A correct letter turns GREEN.")
+            print("A correct letter in the wrong place turns YELLOW.")                
+            print("An incorrect letter turns GRAY.")
             print("Letters can be used more than once.")
             print("Answers are never plurals.")
             print(" ")
@@ -36,7 +58,42 @@ while True:
         print("Invalid input!")
         print(" ")
         
+#main game goes here.............................................................................
 
-#main game goes here...
+i = randint(0, len(wordbank))
+wordle = wordbank[i]
+
+for letter in wordle:
+    wordle_char.append(letter)
+#print(wordle)
+
+for t in range(turns):
+
+    while True:
+        try:
+            print(" ")
+            guess = input("Enter your " + num_terms[n] + " guess: ")
+
+            for g in guess:
+                guess_char.append(g)
+            
+            if guess not in wordbank or len(guess) > 5:
+                print(" ")
+                print("Invalid input!")
+                print(" ")
+            
+            if len(guess) == 5:
+                if guess in wordbank:
+                    n += 1
+                    break
 
 
+        except ValueError:
+            print(" ")
+            print("Invalid input!")
+            print(" ")
+    
+    for i in range(0, 5):
+
+        if guess_char[i] == wordle_char[i]:
+            print(colored(guess_char[i], 'green'), end=" ")
