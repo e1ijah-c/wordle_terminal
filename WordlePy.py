7c062cc0dddb1d173f2ae0b79e96c7014d79f051
Elijah Chia <elcxd99@gmail.com>
2023-10-06 15:23:49 +0800

    finished the game, just need little changes

--- WordlePy.py
+++ WordlePy.py
@@ -1,8 +1,8 @@
+import sys
 import random
 from random import randint
 import colorama
 from colorama import Fore, Style
-
 
 wordbank = []
 wordle_char = []
@@ -11,8 +11,6 @@ num_terms = ["FIRST", "SECOND", "THIRD", "FOURTH", "FIFTH", "LAST"]
 
 guess = ''
 turns = 6
-n = 0
-
 
 with open("Wordle_Dictionary.txt") as file:
     words = file.readlines()
@@ -20,103 +18,195 @@ with open("Wordle_Dictionary.txt") as file:
     for word in words:
         wordbank.append(word.strip('\n'))
 
-while True:
+def main():
+    start()
 
-    try:
+def game():
+    n = 0
+    i = randint(0, len(wordbank))
+    wordle = wordbank[i]
 
-        print(Fore.GREEN + "Welcome to Wordle but in python.")
+    for letter in wordle:
+        wordle_char.append(letter)
+
+    #print(wordle)
+
+    for t in range(turns):
+
+        guess_char.clear()
+
+        while True:
+            try:
+                print(" ")
+                guess = input("Enter your " + num_terms[n] + " guess: ")
+                
+                if guess not in wordbank or len(guess) > 5:
+                    print(" ")
+                    print(Style.BRIGHT + Fore.RED + "Invalid input!")
+                    print(Style.RESET_ALL)
+                
+                if len(guess) == 5:
+                    if guess in wordbank:
+                        for g in guess:
+                            guess_char.append(g)
+                        n += 1
+                        break
+
+            except ValueError:
+                print(" ")
+                print(Style.BRIGHT + Fore.RED + "Invalid input!")
+                print(Style.RESET_ALL)
+        
+        for c in range(len(wordle_char)):
+
+
+
+            if guess_char[c] == wordle_char[c]:
+                print(Fore.GREEN + guess_char[c], end=" ")
+
+            elif guess_char[c] in wordle_char:
+                print(Fore.YELLOW + guess_char[c], end=" ")
+            
+            if guess_char[c] not in wordle_char:
+                print(Style.DIM + Fore.WHITE + guess_char[c], end=" ")
+                print(Style.RESET_ALL, end="")
+
+
+            if c >= len(wordle_char):
+                c = 0
+        
+        print(" ")
         print(Style.RESET_ALL)
 
-        print(" ")
-        print("If you would like to know the rules, input '1'.")
-        print("If you would like to play, input '2'.")
-        print(" ")
-        user_input = int(input("Enter your selected option: "))
+        if guess_char == wordle_char:
 
-        if user_input == 1:
-            print(".")
-            print(".")
-            print(".")
-            print(".")
-            print(".")
-            print("You have to guess the Wordle in 6 goes or less.")
-            print("Every word you enter must be in the word list.")
-            print("A correct letter turns GREEN.")
-            print("A correct letter in the wrong place turns YELLOW.")                
-            print("An incorrect letter turns GRAY.")
-            print("Letters can be used more than once.")
-            print("Answers are never plurals.")
+            n = 0
+            wordle_char.clear()
+
             print(" ")
-            print("Good Luck!")
-            print(" ")
+            print("Congratulations, you got it!")
+            print("Would you like to play again?")
 
-            break
+            while True:
+                try:
+                    print(" ")
+                    print("If you would like to quit, input '1'.")
+                    print("If you would like to play again, input '2'.")
+                    print(" ")
+                    ans = int(input("Enter your selected option: "))
 
-        if user_input == 2:
-            break
+                    if ans == 1:
+                        sys.exit()
 
-    except ValueError:
-        print(" ")
-        print("Invalid input!")
-        print(" ")
-        
-#main game goes here.............................................................................
+                    if ans == 2:
+                        game()
 
-i = randint(0, len(wordbank))
-wordle = wordbank[i]
+                except ValueError:
+                    print(" ")
+                    print(Style.BRIGHT + Fore.RED + "Invalid input!")
+                    print(Style.RESET_ALL)
+    
+    n = 0
+    wordle_char.clear()
 
-for letter in wordle:
-    wordle_char.append(letter)
-
-#print(wordle)
-
-for t in range(turns):
-
-    guess_char.clear()
+    print(" ")
+    print("Sorry you're out of tries, the word was " + wordle + ".")
+    print(" ")
+    print("Would you like to play again?")
 
     while True:
         try:
             print(" ")
-            guess = input("Enter your " + num_terms[n] + " guess: ")
+            print("If you would like to quit, input '1'.")
+            print("If you would like to play again, input '2'.")
+            print(" ")
+            ans = int(input("Enter your selected option: "))
 
-            
-            if guess not in wordbank or len(guess) > 5:
-                print(" ")
-                print("Invalid input!")
-                print(" ")
-            
-            if len(guess) == 5:
-                if guess in wordbank:
-                    for g in guess:
-                        guess_char.append(g)
-                    n += 1
-                    break
+            if ans == 1:
+                sys.exit()
 
+            if ans == 2:
+                game()
 
         except ValueError:
             print(" ")
-            print("Invalid input!")
+            print(Style.BRIGHT + Fore.RED + "Invalid input!")
+            print(Style.RESET_ALL)
+       
+
+def start():
+    while True:
+
+        try:
+
             print(" ")
+            print(Fore.CYAN + Style.BRIGHT + "Welcome to Wordle but in python.")
+            print(Style.RESET_ALL)
+
+            print("If you would like to know the rules, input ", end="") 
+            print(Fore.MAGENTA + "'1'.", end="")
+            print(Style.RESET_ALL)
+            
+            
+            print("If you would like to play, input ", end="")
+            print(Fore.MAGENTA + "'2'.", end="")
+            print(Style.RESET_ALL)
+            
+            
+            print("If you would like to quit, input ", end="")
+            print(Fore.MAGENTA + "'3'.", end="")
+            print(Style.RESET_ALL)
+            
+            print(" ")
+            user_input = int(input("Enter your selected option: "))
+
+            if user_input == 1:
+                print("")
+                print("")
+
+                print("1. You have to guess the Wordle in 6 goes or less.")
+                print(" ")
+                print("2. Every word you enter must be in the word list.")
+                print(Style.RESET_ALL)
+
+                print("3. A correct letter turns ", end="")
+                print(Fore.GREEN + "GREEN")
+                print(Style.RESET_ALL)
+                
+                print("4. A correct letter in the wrong place turns ", end="")  
+                print(Fore.YELLOW + "YELLOW")
+                print(Style.RESET_ALL)              
+                
+                print("5. An incorrect letter turns ", end="")
+                print(Style.DIM + Fore.WHITE + "GREY")
+                print(Style.RESET_ALL)
+
+                print("6. Letters can be used more than once.")
+                print("")
+                print("7. Answers are never plurals.")
+                print(Style.RESET_ALL)
+                print(Style.BRIGHT + Fore.MAGENTA + "Good Luck ;)")
+                print(Style.RESET_ALL)
+
+                game()
+
+
+            if user_input == 2:
+
+                game()
+            
+            if user_input == 3:
+                sys.exit()
+
+        except ValueError:
+            print(" ")
+            print(Style.BRIGHT + Fore.RED + "Invalid input!")
+            print(Style.RESET_ALL)
+
+    return
     
-    for i in range(len(wordle_char)):
-
-        if guess_char[i] == wordle_char[i]:
-            print(Fore.GREEN + guess_char[i], end=" ")
-
-        elif guess_char[i] in wordle_char:
-            print(Fore.YELLOW + guess_char[i], end=" ")
-        
-        if guess_char[i] not in wordle_char:
-            print(Fore.LIGHTWHITE_EX + guess_char[i], end=" ")
     
-    print(" ")
-    print(Style.RESET_ALL)
+while True:
+    main()
 
-    if guess_char == wordle_char:
-        print(" ")
-        print("Congratulations, you got it!")
-        print("Would you like to play again?")
-        break
-
-
-        
+            
